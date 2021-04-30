ad4a964166c89f1b6a6972b7f79a898efc7b00d0
katherinamix <katherinamix@gmail.com>
2021-04-30 16:17:25 +0300

    commit

--- module_0/ugaday_chislo.py
+++ module_0/ugaday_chislo.py
@@ -1,62 +1,53 @@
 import numpy as np
-number = np.random.randint(1,101)      # загадали число
+
+number = np.random.randint(1, 101)  # загадали число
+print ("Загадано число от 1 до 100")
 
 
 def score_game(game_core):
-    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    """Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число"""
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1, 101, size=(1000))
+    random_array = np.random.randint(1, 101, size=1000)
     for number in random_array:
         count_ls.append(game_core(number))
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
-    return (score)
+    return score
 
-def game_core_v1(number):
-    '''Просто угадываем на random, никак не используя информацию о больше или меньше.
-       Функция принимает загаданное число и возвращает число попыток'''
-    count = 0
-    while True:
-        count += 1
-        predict = np.random.randint(1, 101)  # предполагаемое число
-        if number == predict:
-            return count  # выход из цикла, если угадали
 
 def game_core_v2(number):
-    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того,
-    больше оно или меньше нужного.
-       Функция принимает загаданное число и возвращает число попыток'''
+    """Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток"""
     count = 1
-    predict = np.random.randint(1,101)
+    predict = np.random.randint(1, 101)
     while number != predict:
-        count+=1
+        count += 1
         if number > predict:
             predict += 1
         elif number < predict:
             predict -= 1
-    return(count) # выход из цикла, если угадали
+    return count  # выход из цикла, если угадали
+
 
 def game_core_v3(number):
-    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того,
-    больше оно или меньше нужного.
-       Функция принимает загаданное число и возвращает число попыток'''
+    """Решаем методом деления отрезка пополам"""
     count = 1
-    q = 1
-    predict = 50
-    promejutok = 0
+    # находим середину из возможного диапазона - предполагаемое значение
+    predict = 100 // 2
     while number != predict:
-        count+=1
+        count += 1
         if number > predict:
-            promejutok = predict + 1
-            predict = int((promejutok + number) // 2)
+            """Если загаданное число больше величины половины отрезка,
+            то его значение находится в промежутке от предполагаемого значения до самого загаданного числа"""
+            predict = int((predict + 1 + number) // 2)
         elif number < predict:
-            promejutok = predict - 1
-            predict = int((1 + promejutok) // 2)
-    return(count) # выход из цикла, если угадали
+            """Если загаданное число меньше величины половины отрезка, 
+            то его значение находится в промежутке от 1 до предполагаемого значения"""
+            predict = int((predict - 1) // 2)
+    return count  # выход из цикла, если угадали
 
 
-#game_core_v3(number)
 # Проверяем
 score_game(game_core_v2)
 score_game(game_core_v3)
